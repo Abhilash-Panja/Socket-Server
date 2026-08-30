1c848ce85b4b0f90eeb7c6eede763e5634209bc9
Abhilash-Panja <abhilashpanja7@gmail.com>
2026-08-30 19:00:07 +0530

    Added Kafka Consumer

--- src/main/java/com/rideflow/socketserver/Consumers/KafkaConsumerService.java
+++ src/main/java/com/rideflow/socketserver/Consumers/KafkaConsumerService.java
@@ -1,4 +1,4 @@
-package com.example.socketserver.Consumers;
+package com.rideflow.socketserver.Consumers;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
