4202f73139cd93145fcfe7ba6a70983998f0b0eb
Abhilash-Panja <abhilashpanja7@gmail.com>
2026-08-30 19:11:31 +0530

    Added requried files

--- src/main/java/com/rideflow/socketserver/SocketServerApplication.java
+++ src/main/java/com/rideflow/socketserver/SocketServerApplication.java
@@ -2,8 +2,12 @@ package com.rideflow.socketserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.rideflow.rideflowentityservice.models")
+@EnableDiscoveryClient
 public class SocketServerApplication {
 
     public static void main(String[] args) {
